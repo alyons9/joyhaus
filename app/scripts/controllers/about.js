12902fa286b8634bce64fbf17956c0654fb38629
alyons9 <anthony@thelyons.me>
2015-10-24 20:31:50 -0400

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name joyhausApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the joyhausApp
+ */
+angular.module('joyhausApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name joyhausApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the joyhausApp
+ */
+angular.module('joyhausApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
